191f9808053923b5770e1d3177e6ada0f344ac66
Fernando Hickman <fernanhick@gmail.com>
2022-06-18 16:42:34 +0100

    updated projects and components

--- script.js
+++ script.js
@@ -1,42 +1,44 @@
-const navLogo = document.querySelector(".logo");
-navLogo.addEventListener("click", printToConsole);
+const navLogo = document.querySelector('.logo')
+navLogo.addEventListener('click', printToConsole)
 
 function printToConsole() {
-    console.log();
+    console.log()
 }
 
 window.onscroll = function () {
-    scrollFunction();
-};
+    scrollFunction()
+}
 
 function scrollFunction() {
-    let nav = document.getElementById("navbar");
+    let nav = document.getElementById('navbar')
     if (
         document.body.scrollTop > 64 ||
         document.documentElement.scrollTop > 64
     ) {
-        nav.classList.add("scrolled");
+        nav.classList.add('scrolled')
     } else {
-        nav.classList.remove("scrolled");
+        nav.classList.remove('scrolled')
     }
 }
 
-const navButton = document.querySelector(".menu-button");
-navButton.addEventListener("click", toggleMenu);
-let nav = document.getElementById("navbar");
+const navButton = document.querySelector('.menu-button')
+navButton.addEventListener('click', toggleMenu)
+let nav = document.getElementById('navbar')
 
 function toggleMenu() {
-    nav.classList.toggle("nav_toggle");
-    console.log("toggle menu");
+    nav.classList.toggle('nav_toggle')
+    console.log('toggle menu')
 }
 
 //Toggle navbar when clicking any menu button
-linksList = document.querySelectorAll(".link-item");
+linksList = document.querySelectorAll('.link-item')
 for (i = 0; i < linksList.length; i++) {
-    linksList[i].addEventListener("click", function () {
-        nav.classList.remove("nav_toggle");
-    });
+    linksList[i].addEventListener('click', function () {
+        nav.classList.remove('nav_toggle')
+    })
 }
-document.querySelector(".logo").addEventListener("click", function () {
-    nav.classList.remove("nav_toggle");
-});
+//Toggle navbar when clicking any logo button
+document.querySelector('.logo').addEventListener('click', function () {
+    nav.classList.remove('nav_toggle')
+})
+data.title
